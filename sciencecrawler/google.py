c4b02eec33d5ba25a17d42b06128155b566c5c9d
Guilherme ML <guiescsal@gmail.com>
2021-08-15 15:36:00 -0300

    feat: documentação

--- sciencecrawler/google.py
+++ sciencecrawler/google.py
@@ -6,66 +6,6 @@ from bs4 import BeautifulSoup
 from .src.base import SearchBase,ArticleBase
 
 
-class GoogleArticle(ArticleBase):
-    def __init__(self, article_html) -> None:
-        article_response = dict()    
-        self._html = article_html  
-        a_article = article_html.find("h3",{"class":"gs_rt"}).find('a')
-        link = a_article['href']
-        article_response['link'] = link
-        title = a_article.text
-        article_response['title'] = title
-        author = article_html.find('div',{"gs_a"}).text
-        article_response['author'] = author
-        resume = article_html.find('div',{"gs_rs"}).text
-        article_response['resume'] = resume
-        links = article_html.find('div',{"class":"gs_fl"}).find_all('a')
-        article_response['cited'] = None
-        article_response['link_html'] = None
-        for link in links:
-            try:
-                if  "cites" in link['href']:
-                    cited = link['href'].split('&')[0].split('cites=')[-1]
-                    article_response['cited'] = GoogleSearch(None,cited)
-                if "scholar.googleuser" in link['href']:
-                    link_cache = link['href']
-                    article_response['link_html'] = str(requests.get(link_cache).content)
-            except Exception as ex: 
-                pass
-        self._data = article_response
-
-    @property
-    def doi(self):
-        return None
-
-    @property
-    def title(self):
-        return self._data['title']
-
-    @property
-    def authors(self):
-        return self._data['author']
-
-    def get_references(self):
-        return self._data['cited']
-
-    def to_json(self):
-        return self._data
-
-    @property
-    def resume(self):
-        return self._data['resume']
-
-    @property
-    def link_article(self):
-        return self._data['link']
-
-    @property
-    def content_page(self):
-        return self._data['link_html']
-
-    def __repr__(self) -> str:
-        return f'GoogleArticle : {self._data["title"]}'
 
 class GoogleSearch(SearchBase):
     def __init__(self, term, cites = None, language = 'en-us'):
@@ -102,7 +42,7 @@ class GoogleSearch(SearchBase):
             return f'cites: {self._cites} / page: {self._page}'
 
 
-    def get_list_articles(self, interval_requests:int = 0.5) -> List[GoogleArticle]:
+    def get_list_articles(self, interval_requests:int = 0.5):
         if self._page_soup is not None:
             div_main = self._page_soup.find('div',{"id":"gs_res_ccl_mid"})
             if div_main is not None:
@@ -164,3 +104,67 @@ class GoogleSearch(SearchBase):
             self._page_soup = BeautifulSoup(response.content, 'html.parser')
         else:
             self._page_soup = None
+
+
+
+
+class GoogleArticle(ArticleBase):
+    def __init__(self, article_html) -> None:
+        article_response = dict()    
+        self._html = article_html  
+        a_article = article_html.find("h3",{"class":"gs_rt"}).find('a')
+        link = a_article['href']
+        article_response['link'] = link
+        title = a_article.text
+        article_response['title'] = title
+        author = article_html.find('div',{"gs_a"}).text
+        article_response['author'] = author
+        resume = article_html.find('div',{"gs_rs"}).text
+        article_response['resume'] = resume
+        links = article_html.find('div',{"class":"gs_fl"}).find_all('a')
+        article_response['cited'] = None
+        article_response['link_html'] = None
+        for link in links:
+            try:
+                if  "cites" in link['href']:
+                    cited = link['href'].split('&')[0].split('cites=')[-1]
+                    article_response['cited'] = GoogleSearch(None,cited)
+                if "scholar.googleuser" in link['href']:
+                    link_cache = link['href']
+                    article_response['link_html'] = str(requests.get(link_cache).content)
+            except Exception as ex: 
+                pass
+        self._data = article_response
+
+    @property
+    def doi(self):
+        return None
+
+    @property
+    def title(self):
+        return self._data['title']
+
+    @property
+    def authors(self):
+        return self._data['author']
+
+    def get_references(self) -> GoogleSearch:
+        return self._data['cited']
+
+    def to_json(self):
+        return self._data
+
+    @property
+    def resume(self):
+        return self._data['resume']
+
+    @property
+    def link_article(self):
+        return self._data['link']
+
+    @property
+    def content_page(self):
+        return self._data['link_html']
+
+    def __repr__(self) -> str:
+        return f'GoogleArticle : {self._data["title"]}'
